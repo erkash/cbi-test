c6382c6a3c69023fe3518868a4032bf0c7c97b3f
Erkin <erkash88@gmail.com>
2019-11-20 03:40:30 +0600

    Authentication implemented

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,16 +9,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="homepage")
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(ObjectManager $manager)
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $manager->getRepository(User::class)->findAll()
         ]);
     }
 
@@ -40,10 +44,57 @@ class UserController extends AbstractController
             $user->setPassword($hash);
             $manager->persist($user);
             $manager->flush();
+
+            return $this->redirectToRoute('profile', [
+                'id' => $user->getId()
+            ]);
         }
 
         return $this->render('user/registration.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="security_login")
+     * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(Request $request,AuthenticationUtils $authenticationUtils )
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error
+        ]);
+    }
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
+        return $this->render('user/logout.html.twig');
+    }
+
+
+
+    /**
+     * @Route("/user/{id}", name="profile")
+     * @param User $profile
+     */
+    public function profile(User $profile)
+    {
+        if (!$profile) {
+            return $this->redirectToRoute('security_login');
+        }
+
+        return $this->render('user/profile.html.twig', [
+            'profile' => $profile
         ]);
     }
 }
